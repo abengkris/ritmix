da200c0b225ac0ce80948c84c559a746d632f2a3
Abengkris <abengkrischannel@gmail.com>
2025-03-18 03:21:06 +0700

    fix bugs

--- apps/shop/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/api/shop', (req, res) => res.json({ message: 'Shop API' }));
-
-app.listen(3001, () => console.log('Shop on port 3001'));
